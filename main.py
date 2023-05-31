3e720847c8a72c431766ee2d4c89e1e621d10129
akshatlunia <luniaakshat@gmail.com>
2023-05-31 17:51:04 -0500

    Created a way to allow conversation

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 import openai
 
 # Set up your OpenAI API key
-openai.api_key = 'your-api-key'
+openai.api_key = "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"
 
-def generate_chat_completion(prompt):
+def generate_chat_completion(prompt, messages):
     # Set the engine and parameters
-    engine = "davinci"
+    model = "gpt-3.5-turbo"
     max_tokens = 50
 
     # Generate a chat completion
-    response = openai.Completion.create(
-        engine=engine,
-        prompt=prompt,
-        max_tokens=max_tokens
+    response = openai.ChatCompletion.create(
+        model = model,
+        messages = messages,
+        temperature = 0
     )
 
+    messages.append({"role": response.choices[0]["message"]["role"], "content": response.choices[0]["message"]["content"]})
+    print(response.usage)
     # Get the response text
-    completion = response.choices[0].text.strip()
+    completion = response.choices[0]["message"]["content"]
 
     return completion
 
 def main():
-    prompt = "What is the meaning of life?"
+    prompt = "Can you generate a random sql statement?"
+    messages = [
+        {"role": "user", "content": prompt}
+    ]
 
     # Generate a chat completion
-    completion = generate_chat_completion(prompt)
+    while(prompt != "done"):
+        completion = generate_chat_completion(prompt, messages)
+        print("Response:", completion)
+        prompt = input()
+        messages.append({"role": "user", "content": prompt})
 
     # Print the response
-    print("Response:", completion)
+    #print("Response:", completion)
+    print("Messages: ", messages)
 
 if __name__ == '__main__':
     main()
